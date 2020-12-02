c8dd7b710b423ce0df80a46873482794b82808b5
fadiHB <eng.fadi.hb@gmail.com>
2020-12-02 08:26:06 +0200

    add new test

--- tests/challenges/test_binary_search.py
+++ tests/challenges/test_binary_search.py
@@ -10,3 +10,8 @@ def test_odd_num():
     actual = binary_search([1,2,3,4,5],4)
     expected = 3
     assert actual == expected
+
+def test_not_include_num():
+    actual = binary_search([1,2,3,4,5],0)
+    expected = -1
+    assert actual == expected
