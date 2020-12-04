9727d0657c2c24e40fe2dbdd17dee194597342c5
fadiHB <eng.fadi.hb@gmail.com>
2020-12-04 17:42:51 +0200

    done - still readme and some test maybe

--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -1,7 +1,53 @@
-class LinkedList:
-    """
-    Put docstring here
-    """
+# class LinkedList:
+#     """
+#     Put docstring here
+#     """
 
-    # put your LinkedList implementation here
-    pass
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def insert(self, value):
+        node = Node(value)
+        if self.head == None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.size += 1
+
+    def includes(self, value):
+        # node = Node(self.head)
+        if self.size == 0:
+            return False
+        else:
+            current = self.head
+            while current != None:
+                if current.value == value:
+                    return True
+                current = current.next
+            return False
+
+    def __str__(self):
+        current = self.head
+        str1 = ''
+        while current != None:
+            str1 += f'{{ {current.value} }} -> '
+            current = current.next
+        str1 += 'NULL'
+        return str1
+
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.insert(10)
+    ll.insert(5)
+    print(ll.head.next.value)
+    print(ll)
---
+++ data_structures_and_algorithms/data_structures/linked_list/readme.md
@@ -0,0 +1,17 @@
+# Singly Linked List
+
+Linked List
+In computer science, a linked list is a linear collection of data elements whose order is not given by their physical placement in memory. Instead, each element points to the next. It is a data structure consisting of a collection of nodes which together represent a sequence
+
+## Challenge
+insert ,include ,toString.
+we should define these three methood usning Linked-List
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+insert method :
+include mehtod :
+toString method :
+
+## API
+<!-- Description of each method publicly available to your Linked List -->
