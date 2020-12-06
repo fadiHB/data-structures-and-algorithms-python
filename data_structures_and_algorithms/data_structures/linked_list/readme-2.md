05584168b18cfb8841dacf016d9c571981e9b2fc
fadiHB <eng.fadi.hb@gmail.com>
2020-12-06 23:20:29 +0200

    done . still the PR link

---
+++ data_structures_and_algorithms/data_structures/linked_list/readme-2.md
@@ -0,0 +1,19 @@
+# Singly Linked List
+
+Linked List
+In computer science, a linked list is a linear collection of data elements whose order is not given by their physical placement in memory. Instead, each element points to the next. It is a data structure consisting of a collection of nodes which together represent a sequence
+
+## Challenge
+
+append ,insertAfter ,insertBefor.
+we should define these three methood usning Linked-List
+
+## Approach & Efficiency
+
+it took about 6 h
+
+## API
+
+![linked_list](asserts/code-6.jpeg)
+![linked_list](asserts/code-61.jpeg)
+![__str__](asserts/code-62.jpeg)
--- tests/data_structures/test_linked_list.py
+++ tests/data_structures/test_linked_list.py
@@ -1,13 +1,11 @@
 from data_structures_and_algorithms.data_structures.linked_list.linked_list import (
-    LinkedList,Node
+    LinkedList, Node
 )
-
 
 
 def test_instance():
     ll = LinkedList()
     assert isinstance(ll, LinkedList)
-
 
 def test_insert():
     ll = LinkedList()
@@ -31,4 +29,31 @@ def test__str__():
     ll.insert('b')
     ll.insert('a')
     assert ll.__str__() == "{ a } -> { b } -> { c } -> NULL"
-    
+
+def test_append():
+    ll = LinkedList()
+    ll.append(5)
+    assert ll.head.value == 5
+    ll.append(10)
+    assert ll.head.next.value == 10
+    ll.append('f')
+    assert ll.head.next.next.value == 'f'
+
+def test_insertBefore():
+    ll = LinkedList()
+    ll.append(0)
+    ll.append(3)
+    ll.insertBefore(3,5)
+    assert ll.__str__() == "{ 0 } -> { 5 } -> { 3 } -> NULL"
+
+def test_insertAfter():
+    ll = LinkedList()
+    ll.append(0)
+    ll.append(3)
+    ll.insertAfter(3,5)
+    assert ll.__str__() == "{ 0 } -> { 3 } -> { 5 } -> NULL"
+
+
+
+
+
