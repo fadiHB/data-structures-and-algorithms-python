01e4fde47669a662d20a674720b2f3e35b9e76f2
fadiHB <eng.fadi.hb@gmail.com>
2020-12-08 23:30:00 +0200

    not finish yet

--- data_structures_and_algorithms/data_structures/linked_list/linked_list.py
+++ data_structures_and_algorithms/data_structures/linked_list/linked_list.py
@@ -44,10 +44,41 @@ class LinkedList:
         str1 += 'NULL'
         return str1
 
+    def zipLists(self, list1, list2):
+
+
+
+        primary_current = list1.head
+        second_current = list2.head
+
+        while primary_current and second_current :
+
+            primary_current.next  = second_current#
+            second_current.next = primary_current.next.next#
+
+            # second_current.next.next = primary_current
+            # primary_current.next.next = second_current 
+
+            return list1
+
 
 if __name__ == '__main__':
-    ll = LinkedList()
-    ll.insert(10)
-    ll.insert(5)
-    print(ll.head.next.value)
-    print(ll)
+    # ll = LinkedList()
+    # ll.insert(10)
+    # ll.insert(5)
+    # print(ll.head.next.value)
+    # print(ll)
+    ll1 = LinkedList()
+    ll1.insert(1)
+    ll1.insert(2)
+    ll1.insert(3)
+    ll2 = LinkedList()
+    ll2.insert(1)
+    ll2.insert(2)
+    ll2.insert(3)
+    ll3 = LinkedList()
+    ll2.zipLists(ll1,ll2)
+    print(ll1.__str__())
+
+
+    
