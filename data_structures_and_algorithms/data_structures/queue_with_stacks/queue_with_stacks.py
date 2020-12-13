3e4ea178abb9b4f8d7ca723c9b8b9d7931494b95
fadiHB <eng.fadi.hb@gmail.com>
2020-12-13 23:47:54 +0200

    not yet

--- data_structures_and_algorithms/data_structures/queue_with_stacks/queue_with_stacks.py
+++ data_structures_and_algorithms/data_structures/queue_with_stacks/queue_with_stacks.py
@@ -1,2 +1,20 @@
 from data_structures_and_algorithms.data_structures.stacks_and_queues.stacks_and_queues_Class10_1 import Queue
 from data_structures_and_algorithms.data_structures.stacks_and_queues.stacks_and_queues_Class10_2 import Stacks,Node
+
+
+class PseudoQueue :
+    def __init__(self):
+      self.stack1 = Stacks()
+      self.stack2 = Stacks()
+
+
+    def enqueu(self,data):
+         self.stack1.push(data)
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
